add05e904ec5fc9660a460806484ea6cdd236722
Marius Mitea <mmitea@trusimulation.com>
2020-04-13 15:05:57 -0400

    *

--- tester_rpi/Program.cs
+++ tester_rpi/Program.cs
@@ -8,7 +8,9 @@ namespace tester_rpi
 {
     class Program
     {
+        private const int CounterPin = 4;
         private const int ResetPin = 27;
+
         private const int InputPinCount = 5;
         private const int OutputPinCount = 16;
 
@@ -29,24 +31,20 @@ namespace tester_rpi
             for (int i = 0; i < OutputPinCount; ++i) gpio.OpenPin(i + InputPinCount, PinMode.Output);
             gpio.OpenPin(ResetPin, PinMode.Input);
 
-            var inputpairs = Enumerable.Range(0, InputPinCount).Select(idx => new PinValuePair(idx, PinValue.Low)).ToArray();
-            var outputpairs = Enumerable.Range(0, OutputPinCount).Select(idx => new PinValuePair(idx, PinValue.Low)).ToArray();
-
             Stopwatch.Start();
             while (true)
             {
-                gpio.Read(inputpairs);
-                if (inputpairs[4].PinValue == PinValue.High) logic.Reset();
+                //gpio.Read(inputpairs);
+                if (gpio.Read(CounterPin) == PinValue.High) logic.Reset();
 
                 // read the new inputs
                 for (int i = 0; i < InputPinCount; ++i)
-                    inputs[i].Active = inputpairs[i].PinValue == PinValue.High;
+                    inputs[i].Active = gpio.Read(i) == PinValue.High;
 
                 logic.Process();
 
                 // write the outputs
-                outputs.ForEach((w, idx) => outputpairs[idx] = new PinValuePair(idx + InputPinCount, w.Active ? PinValue.High : PinValue.Low));
-                gpio.Write(outputpairs);
+                outputs.ForEach((w, idx) => gpio.Write(idx + InputPinCount, w.Active ? PinValue.High : PinValue.Low));
 
                 ++Cycles;
 
