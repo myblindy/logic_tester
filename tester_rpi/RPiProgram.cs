1d07b5776f16d8343992e0b735ef2622e28376f0
myblindy <tweety.04.01@gmail.com>
2020-05-24 15:31:20 -0400

    last fixes, random input/output mapping

--- tester_rpi/RPiProgram.cs
+++ tester_rpi/RPiProgram.cs
@@ -10,12 +10,12 @@ namespace tester_rpi
 {
     public class RPiProgram
     {
-        private const int ResetPin = 27;
+        //private const int ResetPin = 27;
 
-        private const int InputPin0 = 22;
+        //private const int InputPin0 = 22;
         private static int InputPinCount;
 
-        private const int OutputPin0 = 5;
+        //private const int OutputPin0 = 5;
         private static int OutputPinCount;
 
         static readonly Stopwatch Stopwatch = new Stopwatch();
@@ -37,28 +37,32 @@ namespace tester_rpi
             Logic = new Logic<Light>(TaskScheduler.Default, il => new Light(), ol => new Light());
 #else
             Logic = new Logic<Light>(TaskScheduler.Default, il => new Light(), ol => new Light());
+            var iMapping = new int[] { 5, 6, 13, 19, 26 };
+            var oMapping = new int[] { 12, 20, 21 };
 #endif
 
             InputPinCount = Logic.Inputs.Length;
             OutputPinCount = Logic.Outputs.Length;
 
+            var lastOutputs = new bool[OutputPinCount];
+            var firstIteration = true;
+
             var gpio = new GpioController();
 
             LogicInitialized?.Set();
 
-            for (int i = 0; i < InputPinCount; ++i) gpio.OpenPin(i + InputPin0, PinMode.Input);
-            for (int i = 0; i < OutputPinCount; ++i) if (!(Logic.Outputs[i] is null)) gpio.OpenPin(i + OutputPin0, PinMode.Output);
-            gpio.OpenPin(ResetPin, PinMode.Input);
+            for (int i = 0; i < InputPinCount; ++i) gpio.OpenPin(iMapping[i], PinMode.Input);
+            for (int i = 0; i < OutputPinCount; ++i) if (i < 3) gpio.OpenPin(oMapping[i], PinMode.Output);
+            //gpio.OpenPin(ResetPin, PinMode.Input);
 
             Stopwatch.Start();
             while (true)
             {
-                //gpio.Read(inputpairs);
-                if (gpio.Read(ResetPin) == PinValue.High) Logic.Reset();
+                //if (gpio.Read(ResetPin) == PinValue.High) Logic.Reset();
 
                 // read the new inputs
                 for (int i = 0; i < InputPinCount; ++i)
-                    Logic.Inputs[i].Active = gpio.Read(i + InputPin0) == PinValue.High;
+                    Logic.Inputs[i].Active = gpio.Read(iMapping[i]) == PinValue.High;
 
                 var elapsed = Stopwatch.Elapsed;
                 Logic.Process(elapsed);
@@ -67,14 +71,15 @@ namespace tester_rpi
                 //context.ExecuteAllWorkItems();
 
                 // write the outputs
-                Logic.Outputs.ForEach((w, idx) =>
+                if (firstIteration || !lastOutputs.SequenceEqual(Logic.Outputs.Select(w => w.Active)))
                 {
-                    if (!(Logic.Outputs[idx] is null))
-                        gpio.Write(idx + OutputPin0,
+                    Logic.Outputs.Where((w, idx) => idx < 3).ForEach((w, idx) =>
+                        gpio.Write(oMapping[idx],
                             idx >= 3 ?
                                 w.Active ? PinValue.High : PinValue.Low :
-                                w.Active ? PinValue.Low : PinValue.High);
-                });
+                                w.Active ? PinValue.Low : PinValue.High));
+                    firstIteration = false;
+                }
 
                 ++Cycles;
 
--- tester_rpi_LCD/MainWindow.xaml.cs
+++ tester_rpi_LCD/MainWindow.xaml.cs
@@ -4,6 +4,7 @@ using Avalonia.Markup.Xaml;
 using Avalonia.Threading;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using System;
+using System.Threading.Tasks;
 using tester_rpi_LCD.Model;
 
 namespace tester_rpi_LCD
@@ -20,8 +21,9 @@ namespace tester_rpi_LCD
 #endif
 
             DataContext = new MainModel { Logic = Program.Logic };
-            WindowOpenedEvent.AddClassHandler(typeof(MainWindow), (s, e) => WindowState = WindowState.Maximized);
-            Timer = new DispatcherTimer(TimeSpan.FromMilliseconds(1.0 / 60.0), DispatcherPriority.SystemIdle, (s, e) => ((MainModel)DataContext).UpdateLiveValues());
+            Opened += (s, e) => WindowState = WindowState.Maximized;
+            Timer = new DispatcherTimer(TimeSpan.FromTicks((long)(1.0 / 10.0 * TimeSpan.TicksPerSecond)), DispatcherPriority.SystemIdle,
+                (s, e) => ((MainModel)DataContext).UpdateLiveValues());
         }
 
         private void InitializeComponent()
